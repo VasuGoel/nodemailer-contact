c6b9cfb678bb0ee210b83a27c344f09702aca30b
Vasu Goel <goelvasu0712@gmail.com>
2019-04-17 10:23:58 +0000

    Add initial routes

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // body-parser middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+
+// index route
+app.get("/", (req, res) => {
+    res.redirect("/contact");
+});
+
+// render contact form
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
