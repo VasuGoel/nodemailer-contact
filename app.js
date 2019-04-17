a9e337051432b60b33440c1451aefb28006048a0
Vasu Goel <goelvasu0712@gmail.com>
2019-04-17 10:22:06 +0000

    Add view engine, host public dir, body-parser middleware

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       path = require('path'),
       nodemailer = require('nodemailer');
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+// public directory setup
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
